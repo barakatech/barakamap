249800f7dba4de6494a95cbb5e9ad7ea1176e915
Jorge Aguilera <jorgea@getbaraka.com>
2023-03-01 17:52:37 +0100

    add more people

--- docs/data.js
+++ docs/data.js
@@ -3,6 +3,38 @@ const features = {
         features:[{
             type: "Feature",
             geometry: {
+                type: "Point", coordinates: [55.2274879, 25.0760224]
+            },
+            properties: {
+                popupContent: `Yo! I’m Feras from Dubai, United Arab Emirates`,
+                icon: {
+                    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+                    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+                    iconSize: [25, 41],
+                    iconAnchor: [12, 41],
+                    popupAnchor: [1, -34],
+                    shadowSize: [41, 41]
+                }
+            }            
+        },{
+            type: "Feature",
+            geometry: {
+                type: "Point", coordinates: [72.9460237, 33.6160373]
+            },
+            properties: {
+                popupContent: `Hello, I am Abdul Wahab, from Islamabad, Pakistan`,
+                icon: {
+                    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+                    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+                    iconSize: [25, 41],
+                    iconAnchor: [12, 41],
+                    popupAnchor: [1, -34],
+                    shadowSize: [41, 41]
+                }
+            }            
+        },{
+            type: "Feature",
+            geometry: {
                 type: "Point", coordinates: [-4.7808754, 37.3890936]
             },
             properties: {

638951880bea8d60fefefa395bad8fe2f55070d0
Jorge Aguilera <jorgea@getbaraka.com>
2023-03-01 17:41:41 +0100

    add more people

--- docs/data.js
+++ docs/data.js
@@ -3,6 +3,22 @@ const features = {
         features:[{
             type: "Feature",
             geometry: {
+                type: "Point", coordinates: [-4.7808754, 37.3890936]
+            },
+            properties: {
+                popupContent: `Hi, I'm Cristian, from Puente Genil, Córdoba, Spain`,
+                icon: {
+                    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+                    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+                    iconSize: [25, 41],
+                    iconAnchor: [12, 41],
+                    popupAnchor: [1, -34],
+                    shadowSize: [41, 41]
+                }
+            }            
+        },{
+            type: "Feature",
+            geometry: {
                 type: "Point", coordinates: [-3.6319141, 40.4253096]
             },
             properties: {

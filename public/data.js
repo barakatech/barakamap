e57b31222b09d1a8c2c4efd7c684e760711c8711
Jorge Aguilera <jorgea@getbaraka.com>
2023-03-01 17:23:45 +0100

    first commit

---
+++ public/data.js
@@ -0,0 +1,20 @@
+const features = {
+        type: "FeatureCollection",
+        features:[{
+            type: "Feature",
+            geometry: {
+                type: "Point", coordinates: [-3.6319141, 40.4253096]
+            },
+            properties: {
+                popupContent: `Hi, 
+I’m Jorge, from Madrid, Spain`,
+                icon: {
+                    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+                    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+                    iconSize: [25, 41],
+                    iconAnchor: [12, 41],
+                    popupAnchor: [1, -34],
+                    shadowSize: [41, 41]
+                }
+            }            
+        },]};
